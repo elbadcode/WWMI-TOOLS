5efed06b1ea882eee9a7f676b49eb0eebed79ae4
lobotomyx <lbatvt@gmail.com>
2024-09-03 20:08:35 -0400

    4.2 fix
    these ones arent needed for the fix iirc and I havent quite finished the planned improvements but it shouldnt be worse than before

--- wwmi-tools/blender_export/texture_collector.py
+++ wwmi-tools/blender_export/texture_collector.py
@@ -1,8 +1,8 @@
 import os
 import re
-
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from pathlib import Path
+import subprocess
 
 
 @dataclass
@@ -14,24 +14,64 @@ class Texture:
 
 def get_textures(object_source_folder: Path):
     textures = {}
-    for texture_filename in os.listdir(object_source_folder):
-        if texture_filename.endswith(".dds") or texture_filename.endswith(".jpg"): 
-            # Handle new format
-            hash_pattern = re.compile(r'.*t=([a-f0-9]{8}).*')
-            result = hash_pattern.findall(texture_filename.lower())
-            
+    # scandir is many times faster than listdir and is standard now
+    for texfile in os.scandir(object_source_folder):
+        # I don't think I would use python if not for tuples and walrus operator
+        if (texname := texfile.name.lower()).endswith(
+            (".dds", ".jpg", ".png", ".tga", ".jpeg", ".tiff", 'tif')
+        ):
+            hash_pattern = re.compile(r".*t=([a-f0-9]{8}).*")
+            result = hash_pattern.findall(texname)
             if len(result) != 1:
                 # Handle old format
-                hash_pattern = re.compile(r'.*component_\d-ps-t\d-([a-f0-9]{8}).*')
-                result = hash_pattern.findall(texture_filename.lower())
+                hash_pattern = re.compile(r".*component_\d-ps-t\d-([a-f0-9]{8}).*")
+                result = hash_pattern.findall(texname)
                 if len(result) != 1:
                     continue
-
             texture_hash = result[0]
-
+            # not sure yet how to query other blender addons but ideally should just pass this off to dds addon
+            # blender sucks for dds workflow its generally best to work with uncompressed textures and convert after
+            if not texname.endswith(".dds"):
+                try:
+                    if "texconv" in os.environ["Path"]:
+                        texconv = subprocess.run(
+                            args=[
+                                "texconv",
+                                texfile.path,
+                                "-o",
+                                texfile.path,
+                                "-y",
+                                "-ft",
+                                "dds",
+                            ],
+                            executable="C:/Windows/System32/cmd.exe",
+                            shell=True,
+                        )
+                    elif tconv := os.environ["texconv"] is not None:
+                        if tconv.endswith(".exe"):
+                            texconv = subprocess.run(
+                                args=[
+                                    texfile.path,
+                                    "-o",
+                                    texfile.path,
+                                    "-y",
+                                    "-ft",
+                                    "dds",
+                                ],
+                                executable=tconv,
+                                shell=True,
+                            )
+                        elif os.path.isdir(tconv):
+                            pass
+                except Exception as e:
+                    print(e)
+                texname = os.path.splitext(texname)[0] + ".dds"
             textures[texture_hash] = Texture(
                 hash=texture_hash,
-                path=object_source_folder / texture_filename,
-                filename=texture_filename,
+                # usually I pass only the file entry since you can access path or name from a single var,
+                # but there's no harm done unless we have lots of variables to remember and you got to meet the walrus
+                path=Path(texfile.path),
+                filename=texname,
             )
+
     return list(textures.values())
